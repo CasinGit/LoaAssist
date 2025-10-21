737727f5d54c30715b329b921dfca7612de88d30
Casin <107905043+CasinGit@users.noreply.github.com>
2025-10-21 20:38:33 +0900

    chore(release): bump version to 1.2.4
    DB 테이블 초기화 작업이 실행되지 않던 현상 핫픽스 진행

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,3 +3,14 @@
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
+
+import type { LayoutLoad } from "./$types";
+
+import { initializerDB } from "$lib/db";
+
+export const load: LayoutLoad = async () => {
+    // DB 초기화가 끝날 때까지 기다림
+    await initializerDB();
+
+    return {};
+};
